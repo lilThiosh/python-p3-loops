06cf57d76cf05cc3f610063bd7738dab27515f4b
Ian <thiongoian6148@gmail.com>
2024-06-04 09:08:27 +0300

    Python loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
+
+
+def fizzbuzz(num):
+     for number in range(1 , 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print( "FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+def reverse_string(str):
+    reversed_str = ""
+    for char in str:
+        reversed_str = char + reversed_str
+    return reversed_str
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
-def fizzbuzz():
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
